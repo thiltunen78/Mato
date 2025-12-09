9d7bc3c9acd20cb0892d67c92200224e1c5a6a4f
Tero Hiltunen <termikhil@gmail.com>
2025-12-09 10:22:25 +0200

    add formatting changes

--- Mato.ino
+++ Mato.ino
@@ -7,7 +7,6 @@
 //for tm1637
 #define CLK 5
 #define DIO 4
-
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     4
@@ -15,8 +14,8 @@
 #define DIR_LEFT 2
 #define DIR_UP 3
 #define DIR_DOWN 4
-#define INITIAL_MAINLOOP_DELAY 80
-#define MIN_MAINLOOP_DELAY 40
+#define INITIAL_MAINLOOP_DELAY 70
+#define MIN_MAINLOOP_DELAY 20
 #define WORM_MAX_LEN 30
 #define RIGHT_BUTTON_PIN 12
 #define LEFT_BUTTON_PIN 2
@@ -36,7 +35,6 @@ struct EEPROMDATA
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 TM1637Display tm1637display(CLK, DIO);
-
 bool rightButtonDown = false;
 bool leftButtonDown = false;
 byte dir = DIR_RIGHT;
@@ -54,7 +52,6 @@ unsigned int mainLoopId = 0;
 byte resetCount = 0;
 byte livesLeft = 3;
 EEPROMDATA edata{0,1};
-
 XY xy{0,0};
 LinkedList<XY> linkedlist;
 
@@ -69,25 +66,21 @@ void setup()
   
   EEPROM.get(0, edata); 
 
-  if(edata.hscore > 10000)
-  {
+  if (edata.hscore > 10000) {
     edata.hscore = 0;
     EEPROM.put(0, edata);
   }
 
-  if(edata.hlevel > 10000)
-  {
+  if (edata.hlevel > 10000) {
     edata.hlevel = 1;
     EEPROM.put(0, edata);
   }
   
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
-  {   
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {   
     for(;;); // Don't proceed, loop forever
   }
   
-  display.clearDisplay();
-  
+  display.clearDisplay();  
   display.display();
 
   tone(BUZZER_PIN,450,200);
@@ -108,46 +101,39 @@ void loop()
 
 void setMainLoopRunning()
 {
-  if(startFromBeginning)
-  {
+  if (startFromBeginning) {
     mainLoopId = taskManager.scheduleFixedRate(INITIAL_MAINLOOP_DELAY, mainLoop);
   }
-  else
-  {  
+  else {  
     mainLoopId = taskManager.scheduleFixedRate(mainLoopDelay, mainLoop);
   }
 }
 
 void mainLoop()
 { 
-  start();    
-  
+  start();  
   setPosition();
-
-  handleCollision();
-  
+  handleCollision();  
   drawWorm();
 }
 
 void rightSwitchLoop()
 {
-  if(startFromBeginning)
+  if (startFromBeginning)
     return;
     
-  if(digitalRead(RIGHT_BUTTON_PIN))
-  {
-    if(!rightButtonDown)
-    {
-      if(dir == DIR_RIGHT)
+  if (digitalRead(RIGHT_BUTTON_PIN)) {
+    if (!rightButtonDown) {
+      if (dir == DIR_RIGHT)
         dir = DIR_DOWN;
-      else if(dir == DIR_DOWN)
+      else if (dir == DIR_DOWN)
         dir = DIR_LEFT;
-      else if(dir == DIR_LEFT)
+      else if (dir == DIR_LEFT)
         dir = DIR_UP;
       else
         dir = DIR_RIGHT;  
 
-      if(currentScore > 1)
+      if (currentScore > 1)
         currentScore--;
 
       tone(BUZZER_PIN,300,30);  
@@ -160,27 +146,24 @@ void rightSwitchLoop()
 
 void leftSwitchLoop()
 {
-  if(startFromBeginning)
+  if (startFromBeginning)
     return;
     
-  if(digitalRead(LEFT_BUTTON_PIN))
-  {
-    if(!leftButtonDown)
-    {
-      if(dir == DIR_RIGHT)
+  if (digitalRead(LEFT_BUTTON_PIN)) {
+    if (!leftButtonDown) {
+      if (dir == DIR_RIGHT)
         dir = DIR_UP;
-      else if(dir == DIR_DOWN)
+      else if (dir == DIR_DOWN)
         dir = DIR_RIGHT;
-      else if(dir == DIR_LEFT)
+      else if (dir == DIR_LEFT)
         dir = DIR_DOWN;
       else
         dir = DIR_LEFT;  
 
-      if(currentScore > 1)
+      if (currentScore > 1)
         currentScore--;
 
       tone(BUZZER_PIN,300,30);
-
       resetCount = 0; // reset high sore done only with right button
     }    
     leftButtonDown = true;    
@@ -191,8 +174,7 @@ void leftSwitchLoop()
 
 void start()
 {
-  if(startFromBeginning)
-  {
+  if (startFromBeginning) {
     tm1637display.clear();
     display.clearDisplay();    
     display.setTextSize(3); 
@@ -211,27 +193,47 @@ void start()
     display.setCursor(10, 55);
     display.println(F("Highest level:"));
     display.setCursor(97, 55);
-    display.println(edata.hlevel);
-   
+    display.println(edata.hlevel);   
     display.display();  
     
-    while(!digitalRead(RIGHT_BUTTON_PIN) && !digitalRead(LEFT_BUTTON_PIN)){      
+    while (!digitalRead(RIGHT_BUTTON_PIN) && !digitalRead(LEFT_BUTTON_PIN)) {      
     }    
+
+    delay(500);
+    
+    display.clearDisplay();
+    display.setTextSize(1); 
+    display.setTextColor(WHITE);
+    display.setCursor(25, 33);
+    display.println(F("left -> fast"));
+    display.setCursor(25, 45);
+    display.println(F("right -> slow"));
+    display.display();
+
+    bool bLeftButton = false;
+    bool bRightButton = false;
+    
+    do {
+      bLeftButton = digitalRead(LEFT_BUTTON_PIN);
+      bRightButton = digitalRead(RIGHT_BUTTON_PIN);       
+    }
+    while (!bRightButton && !bLeftButton);
     
     delay(500);
     
     display.clearDisplay();
-
     tm1637display.showNumberDec(0, false);
     tm1637display.showNumberDec(0, false);
-
-    appleposx = random(0,31);
-    appleposy = random(0, 15);
-    drawBigPixel(appleposx, appleposy, WHITE);
-  
+    drawApple();  
     display.display();
 
     startFromBeginning = false;
+
+    if (bLeftButton) { // speed up worm if left button is pressed 
+      mainLoopDelay -= 20;
+      taskManager.cancelTask(mainLoopId);
+      taskManager.scheduleOnce(500, setMainLoopRunning);        
+    }
   }
 }
 
@@ -265,32 +267,24 @@ bool getBigPixel(const byte &x, const byte &y)
 
 void setPosition()
 {
-  if(dir == DIR_RIGHT)
-  {
-    posx++;   
-
-    if(posx > 31)
+  if (dir == DIR_RIGHT) {
+    posx++;
+    if (posx > 31)
       posx = 0;
   }
-  else if(dir == DIR_LEFT)
-  {
+  else if (dir == DIR_LEFT) {
     posx--;
-
-    if(posx < 0)
+    if (posx < 0)
       posx = 31;
   }
-  else if(dir == DIR_UP)
-  {
-    posy--;  
-
-    if(posy < 0)
+  else if (dir == DIR_UP) {
+    posy--;
+    if (posy < 0)
       posy = 15;
   }
-  else
-  {
+  else {
     posy++;
-
-    if(posy > 15)
+    if (posy > 15)
       posy = 0;
   }  
 }
@@ -298,17 +292,14 @@ void setPosition()
 // reset highscore if there are 10 one point scores in a row in leve 1 using only right button.
 void handleHighScoreReset()
 {
-  if(level > 1) // only in level 1
+  if (level > 1) // only in level 1
     return;
     
-  if(currentScore == 1)
-  {    
+  if (currentScore == 1) {    
     resetCount++;
-    if(resetCount == 10)
-    {
+    if (resetCount == 10) {
       edata.hscore = 0;
-      edata.hlevel = 1;
-      
+      edata.hlevel = 1;      
       EEPROM.put(0, edata);
       resetValues(true);
     }
@@ -327,8 +318,7 @@ void gameOver()
   display.setCursor(7,30);    
   display.setTextSize(1);
     
-  if(score > edata.hscore)
-  {
+  if (score > edata.hscore) {
     edata.hscore = score;
     EEPROM.put(0, edata);
     
@@ -340,8 +330,7 @@ void gameOver()
     display.setCursor(45,56);
     display.println(level);
   }
-  else
-  {
+  else {
     display.println(F("Score: "));
     display.setCursor(45,30);
     display.println(score);
@@ -353,19 +342,17 @@ void gameOver()
 
   display.display();
 
-  if(level > edata.hlevel)
-  {
+  if (level > edata.hlevel) {
     edata.hlevel = level;
     EEPROM.put(0, edata);    
   }
   
-  for (int i = 1000;i > 0;i=(i-10))
-  {
+  for (int i = 1000;i > 0;i=(i-10)) {
     tone(BUZZER_PIN,i,20);
     delay(20);
   }
   
-  while(!digitalRead(RIGHT_BUTTON_PIN) && !digitalRead(LEFT_BUTTON_PIN)){
+  while (!digitalRead(RIGHT_BUTTON_PIN) && !digitalRead(LEFT_BUTTON_PIN)) {
   }
 
   delay(500);
@@ -375,12 +362,10 @@ void gameOver()
 
 void lifeLoss()
 {
-  flashScreen();
-  
+  flashScreen();  
   livesLeft--;
 
-  if(livesLeft == 0)
-  {    
+  if (livesLeft == 0) {    
     gameOver();
     return;  
   }  
@@ -390,9 +375,9 @@ void lifeLoss()
   display.setTextColor(WHITE);        
   display.setCursor(30,25);
   
-  if(livesLeft == 2)             
+  if (livesLeft == 2)             
     display.println(F("2 lives left"));
-  else if(livesLeft == 1)
+  else if (livesLeft == 1)
     display.println(F("1 life left"));
   
   display.display();
@@ -409,8 +394,7 @@ void lifeLoss()
 
 void flashScreen()
 {
-  for(int i=0;i<15;i++)
-  {
+  for (int i=0;i<15;i++) {
     display.invertDisplay(true);
     display.display();
     tone(BUZZER_PIN,200,50);
@@ -424,18 +408,16 @@ void flashScreen()
 
 void handleCollision()
 {
-  if((posx == appleposx) && (posy == appleposy))
-  {    
+  if ((posx == appleposx) && (posy == appleposy)) {    
     tone(BUZZER_PIN,700,200);
     
-    if(wormLen < WORM_MAX_LEN)
+    if (wormLen < WORM_MAX_LEN)
       wormLen++;
 
     handleHighScoreReset();
 
     //bonus points if only 2 or less turns
-    if(currentScore >= 8)
-    {
+    if (currentScore >= 8) {
       tone(BUZZER_PIN,900,200);
       currentScore += 5;
     }
@@ -445,15 +427,13 @@ void handleCollision()
         
     tm1637display.showNumberDec(score, false);
 
-    if(wormLen == WORM_MAX_LEN)
-    {
+    if (wormLen == WORM_MAX_LEN) {
       nextLevel();
     }
     
     drawApple();
   }
-  else if(getBigPixel(posx,posy))
-  {
+  else if (getBigPixel(posx,posy)) {
     lifeLoss();  
   }
 }
@@ -462,27 +442,24 @@ void drawApple()
 {
   bool clearSpace = true;
     
-  do   
-  {
+  do {
     appleposx = random(0,31);
     appleposy = random(0, 15);
 
-    if(getBigPixel(appleposx,appleposy))
-    {
+    if (getBigPixel(appleposx,appleposy)) {
       clearSpace = false;
     }
-    else
-    {
+    else {
       clearSpace = true;
       drawBigPixel(appleposx, appleposy, WHITE);
     }
   }
-  while(clearSpace == false);
+  while (clearSpace == false);
 }
 
 void drawWorm()
 {
-  if(startFromBeginning)
+  if (startFromBeginning)
     return;
   
   drawBigPixel(posx, posy, WHITE);
@@ -492,8 +469,7 @@ void drawWorm()
 
   linkedlist.InsertHead(xy);
 
-  if(linkedlist.GetSize() > wormLen)
-  {
+  if (linkedlist.GetSize() > wormLen) {
     xy = linkedlist.GetTail();  
     drawBigPixel(xy.x, xy.y, BLACK);
     linkedlist.RemoveTail();           
@@ -512,8 +488,7 @@ void resetValues(bool fullReset)
   posy = 0;
   currentScore = 10;  
 
-  if(fullReset)
-  {
+  if (fullReset) {
     startFromBeginning = true;
     score = 0;
     level = 1;
@@ -540,15 +515,14 @@ void nextLevel()
   display.setCursor(40,5);             
   display.println(F("Level"));
 
-  if(level < 10)
+  if (level < 10)
     display.setCursor(60,30);             
   else
     display.setCursor(57,30);
   
   display.println(level);
 
-  if(level > edata.hlevel)
-  {
+  if (level > edata.hlevel) {
     display.setTextSize(1);             
     display.setTextColor(WHITE);        
     display.setCursor(10,55);             
@@ -557,8 +531,7 @@ void nextLevel()
   
   display.display();
 
-  for (int i = 200;i < 1000;i=(i+20))
-  {
+  for (int i = 200;i < 1000;i=(i+20)) {
     tone(BUZZER_PIN,i,20);
     delay(20);
   }
@@ -570,7 +543,7 @@ void nextLevel()
 
   drawLevel();
   
-  if(mainLoopDelay > MIN_MAINLOOP_DELAY)
+  if (mainLoopDelay > MIN_MAINLOOP_DELAY)
     mainLoopDelay = mainLoopDelay - 5;
 
   taskManager.cancelTask(mainLoopId);
@@ -579,51 +552,39 @@ void nextLevel()
 
 void drawLevel()
 {
-  if(level > 1)
-  {
-    for(byte i=5;i<10;i++)
-    {
+  if (level > 1) {
+    for (byte i=5;i<10;i++) {
       drawBigPixel(15, i, WHITE);
       drawBigPixel(16, i, WHITE);
     }
 
-    if(level > 2)
-    {
-      for(byte i=23;i<28;i++)
-      {
+    if (level > 2) {
+      for (byte i=23;i<28;i++) {
         drawBigPixel(i, 7, WHITE);
         drawBigPixel(i, 8, WHITE);
       }  
 
-      if(level > 3)
-      {
-        for(byte i=4;i<9;i++)
-        {
+      if (level > 3) {
+        for(byte i=4;i<9;i++) {
           drawBigPixel(i, 7, WHITE);
           drawBigPixel(i, 8, WHITE);
         }
 
-        if(level > 4)
-        {
-          for(byte i=5;i<10;i++)
-          {
+        if (level > 4) {
+          for (byte i=5;i<10;i++) {
             drawBigPixel(13, i, WHITE);
             drawBigPixel(14, i, WHITE);
             drawBigPixel(17, i, WHITE);
             drawBigPixel(18, i, WHITE);
           }
-          if(level > 5)
-          {
-            for(byte i=23;i<28;i++)
-            {
+          if (level > 5) {
+            for (byte i=23;i<28;i++) {
               drawBigPixel(i, 12, WHITE);
               drawBigPixel(i, 13, WHITE);
             }
 
-            if(level > 6)
-            {
-              for(byte i=4;i<9;i++)
-              {
+            if (level > 6) {
+              for (byte i=4;i<9;i++) {
                 drawBigPixel(i, 3, WHITE);
                 drawBigPixel(i, 2, WHITE);
               }  
